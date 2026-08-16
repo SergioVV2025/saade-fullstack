bfc71cafa72aafa451acd746e78d711a52fefce4
Sergio V <verasteguisergio@hotmail.com>
2026-08-16 16:16:23 -0600

    refactor: use environment variable for MongoDB connection

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,10 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/reservations");
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("Conectado correctamente a MongoDB!!!");
   } catch (error) {
-    // console.log("Error conectándose a la DB:", err.message);
     console.log("Error conectándose a la DB!!!");
   }
 };
